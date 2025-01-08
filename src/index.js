73c23cb2c99354ed9d8e97e5bd02ff2dcda39794
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-01-08 23:59:17 +0000

    Create some test project code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Button = () => {
+    const [color, setColor] = useState('blue');
+
+    const changeColor = () => {
+        setColor(color === 'blue' ? 'red' : 'blue');
+    };
+
+    return (
+        <button style={{ backgroundColor: color }} onClick={changeColor}>
+            Click Me
+        </button>
+    )
+}
