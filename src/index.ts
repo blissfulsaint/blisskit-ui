e778e5452e869b66fb016646d6bb98b83c1c6603
Brandon Lisonbee <blisonbee213596@gmail.com>
2025-01-27 20:17:31 -0700

    Update exports / Add /dist to gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Separator } from './components/Separator/Separator';
 export { default as LayoutBand } from './components/LayoutBand/LayoutBand';
+export { default as CardLink } from './components/CardLink/CardLink';
