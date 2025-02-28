73471f943b9183a7b8280674e7fa3eebc720f509
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-02-28 00:03:28 +0000

    Create Form and OutlineFieldset component, begin testing

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from './LayoutBand';
 export * from './Separator';
 export * from './CardLink';
 export * from './StatusMessage';
+export * from './Form';
+export * from './OutlineFieldset';
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { default as Separator } from './components/Separator/Separator';
 export { default as LayoutBand } from './components/LayoutBand/LayoutBand';
 export { default as CardLink } from './components/CardLink/CardLink';
 export { default as StatusMessage } from './components/StatusMessage/StatusMessage';
+export { default as Form } from './components/Form/Form';
+export { default as OutlineFieldset } from './components/OutlineFieldset/OutlineFieldset';
