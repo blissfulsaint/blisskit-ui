6e361a3491c05ffa009db5fdb4c0898574bf7400
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-02-28 19:50:49 +0000

    Add new pattern to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
   plugins: [],
   safelist: [
     { pattern: /(border|text|bg)-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose|slate|gray|zinc|neutral|stone)/ },
+    { pattern: /hover:text-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose|slate|gray|zinc|neutral|stone)/ },
   ],
 };
 export default config;
